a073e14b48dec3635d043523346feb77782322dc
Graham Fairweather <graham.fairweather@ispy.se>
2017-03-27 14:23:30 +0200

    Remove yarn and jscs, cleanup

--- index.js
+++ index.js
@@ -22,39 +22,14 @@
  * alt="npm version" height="18">
  * </a>
  *
- * getFunctionName module. Returns the name of the function.
+ * Get the name of the function.
  *
- * <h2>ECMAScript compatibility shims for legacy JavaScript engines</h2>
- * `es5-shim.js` monkey-patches a JavaScript context to contain all EcmaScript 5
- * methods that can be faithfully emulated with a legacy JavaScript engine.
- *
- * `es5-sham.js` monkey-patches other ES5 methods as closely as possible.
- * For these methods, as closely as possible to ES5 is not very close.
- * Many of these shams are intended only to allow code to be written to ES5
- * without causing run-time errors in older engines. In many cases,
- * this means that these shams cause many ES5 methods to silently fail.
- * Decide carefully whether this is what you want. Note: es5-sham.js requires
- * es5-shim.js to be able to work properly.
- *
- * `json3.js` monkey-patches the EcmaScript 5 JSON implimentation faithfully.
- *
- * `es6.shim.js` provides compatibility shims so that legacy JavaScript engines
- * behave as closely as possible to ECMAScript 6 (Harmony).
- *
- * @version 1.1.0
+ * @version 1.1.1
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
  * @module get-function-name-x
  */
-
-/* jslint maxlen:80, es6:true, white:true */
-
-/* jshint bitwise:true, camelcase:true, curly:true, eqeqeq:true, forin:true,
-   freeze:true, futurehostile:true, latedef:true, newcap:true, nocomma:true,
-   nonbsp:true, singleGroups:true, strict:true, undef:true, unused:true,
-   es3:false, esnext:true, plusplus:true, maxparams:1, maxdepth:1,
-   maxstatements:3, maxcomplexity:2 */
 
 /* eslint strict: 1, max-statements: 1, func-style: 1, no-new-func: 1 */
 
@@ -71,7 +46,7 @@
     /* jshint evil:true */
     var test2 = new Function();
     if (test2.name === 'anonymous') {
-      getFnName = function (fn) {
+      getFnName = function getName(fn) {
         return fn.name && fn.name !== 'anonymous' ? fn.name : '';
       };
     }
@@ -79,13 +54,11 @@
   } else {
     var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
     var fToString = Function.prototype.toString;
-    var pMatch = String.prototype.match;
-    var pReplace = String.prototype.replace;
     var s = require('white-space-x').ws;
     var x = '^[' + s + ']*(?:function|class)[' + s + ']*\\*?[' + s + ']+([\\w\\$]+)[' + s + ']*';
     var reName = new RegExp(x, 'i');
-    getFnName = function (fn) {
-      var name = pMatch.call(pReplace.call(fToString.call(fn), STRIP_COMMENTS, ' '), reName);
+    getFnName = function getName(fn) {
+      var name = fToString.call(fn).replace(STRIP_COMMENTS, ' ').match(reName);
       return name && name[1] !== 'anonymous' ? name[1] : '';
     };
   }
