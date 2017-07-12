e516b2795bea83b6cbffe4afcf8028456ba4a345
Xotic750 <xotic750@gmail.com>
2017-07-12 18:11:32 +0200

    Update dependencies

--- index.js
+++ index.js
@@ -1,92 +1,86 @@
 /**
- * @file
- * <a href="https://travis-ci.org/Xotic750/get-function-name-x"
- * title="Travis status">
- * <img
- * src="https://travis-ci.org/Xotic750/get-function-name-x.svg?branch=master"
- * alt="Travis status" height="18">
- * </a>
- * <a href="https://david-dm.org/Xotic750/get-function-name-x"
- * title="Dependency status">
- * <img src="https://david-dm.org/Xotic750/get-function-name-x.svg"
- * alt="Dependency status" height="18"/>
- * </a>
- * <a
- * href="https://david-dm.org/Xotic750/get-function-name-x#info=devDependencies"
- * title="devDependency status">
- * <img src="https://david-dm.org/Xotic750/get-function-name-x/dev-status.svg"
- * alt="devDependency status" height="18"/>
- * </a>
- * <a href="https://badge.fury.io/js/get-function-name-x" title="npm version">
- * <img src="https://badge.fury.io/js/get-function-name-x.svg"
- * alt="npm version" height="18">
- * </a>
- *
- * Get the name of the function.
- *
- * @version 1.2.0
+ * @file Get the name of the function.
+ * @version 2.0.0
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
  * @module get-function-name-x
  */
 
-/* eslint strict: 1, max-statements: 1, func-style: 1, no-new-func: 1 */
+'use strict';
 
-/* global module */
+var isFunctionLike;
+try {
+  // eslint-disable-next-line no-new-func
+  new Function('"use strict"; return class My {};')();
+  isFunctionLike = function _isFunctionLike(value) {
+    return typeof value === 'function';
+  };
+} catch (ignore) {
+  isFunctionLike = require('is-function-x');
+}
 
-;(function () { // eslint-disable-line no-extra-semi
-
-  'use strict';
-
-  var isFunction = require('is-function-x');
-  var getFnName;
-  var t = function test1() {};
-  if (t.name === 'test1') {
-    /* jshint evil:true */
-    var test2 = new Function();
-    if (test2.name === 'anonymous') {
-      getFnName = function getName(fn) {
-        return fn.name && fn.name !== 'anonymous' ? fn.name : '';
-      };
-    }
-
-  } else {
-    var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
-    var fToString = Function.prototype.toString;
-    var s = require('white-space-x').ws;
-    var x = '^[' + s + ']*(?:function|class)[' + s + ']*\\*?[' + s + ']+([\\w\\$]+)[' + s + ']*';
-    var reName = new RegExp(x, 'i');
+var getFnName;
+var t = function test1() {};
+if (t.name === 'test1') {
+  // eslint-disable-next-line no-new-func
+  var test2 = new Function();
+  if (test2.name === 'anonymous') {
     getFnName = function getName(fn) {
-      var name = fToString.call(fn).replace(STRIP_COMMENTS, ' ').match(reName);
-      return name && name[1] !== 'anonymous' ? name[1] : '';
+      return fn.name === 'anonymous' ? '' : fn.name;
+    };
+  } else {
+    getFnName = function getName(fn) {
+      return fn.name;
     };
   }
-  /**
-   * This method returns the name of the function, or `undefined` if not
-   * a function.
-   *
-   * @param {Function} fn The function to get the name of.
-   * @return {undefined|string} The name of the function,  or `undefined` if
-   *  not a function.
-   * @example
-   * var getFunctionName = require('get-function-name-x');
-   *
-   * getFunctionName(); // undefined
-   * getFunctionName(Number.MIN_VALUE); // undefined
-   * getFunctionName('abc'); // undefined
-   * getFunctionName(true); // undefined
-   * getFunctionName({ name: 'abc' }); // undefined
-   * getFunctionName(function () {}); // ''
-   * getFunctionName(new Function ()); // ''
-   * getFunctionName(function test1() {}); // 'test1'
-   * getFunctionName(function* test2() {}); // 'test2'
-   * getFunctionName(class Test {}); // 'Test'
-   */
-  module.exports = function getFunctionName(fn) {
-    if (!isFunction(fn)) {
-      return void 0;
+} else {
+  var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+  var fToString = Function.prototype.toString;
+  var s = require('white-space-x').string;
+  var x = '^[' + s + ']*(?:async)?[' + s + ']*(?:function|class)[' + s + ']*\\*?[' + s + ']+([\\w\\$]+)[' + s + ']*';
+  var reName = new RegExp(x, 'i');
+  getFnName = function getName(fn) {
+    var match;
+    try {
+      var str = fToString.call(fn).replace(STRIP_COMMENTS, ' ');
+      match = str.match(reName);
+    } catch (e) {}
+
+    if (match) {
+      var name = match[1];
+      return name === 'anonymous' ? '' : name;
     }
-    return getFnName ? getFnName(fn) : fn.name;
+
+    return '';
   };
-}());
+}
+
+/**
+ * This method returns the name of the function, or `undefined` if not
+ * a function.
+ *
+ * @param {Function} fn - The function to get the name of.
+ * @returns {undefined|string} The name of the function,  or `undefined` if
+ *  not a function.
+ * @example
+ * var getFunctionName = require('get-function-name-x');
+ *
+ * getFunctionName(); // undefined
+ * getFunctionName(Number.MIN_VALUE); // undefined
+ * getFunctionName('abc'); // undefined
+ * getFunctionName(true); // undefined
+ * getFunctionName({ name: 'abc' }); // undefined
+ * getFunctionName(function () {}); // ''
+ * getFunctionName(new Function ()); // ''
+ * getFunctionName(function test1() {}); // 'test1'
+ * getFunctionName(function* test2() {}); // 'test2'
+ * getFunctionName(class Test {}); // 'Test'
+ */
+module.exports = function getFunctionName(fn) {
+  if (isFunctionLike(fn) === false) {
+    return void 0;
+  }
+
+  return getFnName ? getFnName(fn) : fn.name;
+};
--- tests/spec/test.js
+++ tests/spec/test.js
@@ -1,134 +1,143 @@
-/* eslint strict: 1, max-lines: 1, symbol-description: 1, max-nested-callbacks: 1,
-   max-statements: 1, no-new-func: 1 */
+'use strict';
 
-/* global JSON:true, expect, module, require, describe, it, xit, returnExports */
-
-;(function () { // eslint-disable-line no-extra-semi
-
-  'use strict';
-
-  var getFunctionName;
-  if (typeof module === 'object' && module.exports) {
-    require('es5-shim');
-    require('es5-shim/es5-sham');
-    if (typeof JSON === 'undefined') {
-      JSON = {};
-    }
-    require('json3').runInContext(null, JSON);
-    require('es6-shim');
-    getFunctionName = require('../../index.js');
-  } else {
-    getFunctionName = returnExports;
+var getFunctionName;
+if (typeof module === 'object' && module.exports) {
+  require('es5-shim');
+  require('es5-shim/es5-sham');
+  if (typeof JSON === 'undefined') {
+    JSON = {};
   }
+  require('json3').runInContext(null, JSON);
+  require('es6-shim');
+  getFunctionName = require('../../index.js');
+} else {
+  getFunctionName = returnExports;
+}
 
-  var getFat = function getFatFunc() {
-    try {
-      return new Function('return () => {return this;};')();
-    } catch (ignore) {}
-    return false;
-  };
+var getFat = function getFatFunc() {
+  try {
+    // eslint-disable-next-line no-new-func
+    return new Function('return () => {return this;};')();
+  } catch (ignore) {}
+  return false;
+};
 
-  var ifSupportsFatit = getFat() ? it : xit;
+var ifSupportsFatit = getFat() ? it : xit;
 
-  var getGF = function getGeneratoFunc() {
-    try {
-      return new Function('return function* idMaker(){};')();
-    } catch (ignore) {}
-    return false;
-  };
+var getGF = function getGeneratoFunc() {
+  try {
+    // eslint-disable-next-line no-new-func
+    return new Function('return function* idMaker(){};')();
+  } catch (ignore) {}
+  return false;
+};
 
-  var ifSupportsGFit = getGF() ? it : xit;
+var ifSupportsGFit = getGF() ? it : xit;
 
-  var getC = function getClassFunc() {
-    try {
-      return new Function('"use strict"; return class My {};')();
-    } catch (ignore) {}
-    return false;
-  };
+var getC = function getClassFunc() {
+  try {
+    // eslint-disable-next-line no-new-func
+    return new Function('"use strict"; return class My {};')();
+  } catch (ignore) {}
+  return false;
+};
 
-  var ifSupportsCit = getC() ? it : xit;
+var ifSupportsCit = getC() ? it : xit;
 
-  var getAF = function getAsyncFunc() {
-    try {
-      return new Function('return async function wait() {}')();
-    } catch (ignore) {}
-    return false;
-  };
+var getAF = function getAsyncFunc() {
+  try {
+    // eslint-disable-next-line no-new-func
+    return new Function('return async function wait() {}')();
+  } catch (ignore) {}
+  return false;
+};
 
-  var ifSupportsAFit = getAF() ? it : xit;
+var ifSupportsAFit = getAF() ? it : xit;
 
-  describe('Basic tests', function () {
-    it('should return `undefined` for everything', function () {
-      var values = [true, 'abc', 1, null, undefined, new Date(), [], /r/];
-      var cb = function () {};
-      var expected = values.map(cb);
-      var actual = values.map(getFunctionName);
-      expect(actual).toEqual(expected);
-    });
+describe('Basic tests', function () {
+  it('should return `undefined` for everything', function () {
+    var values = [
+      true,
+      'abc',
+      1,
+      null,
+      undefined,
+      new Date(),
+      [],
+      /r/
+    ];
 
-    it('should return a correct string for everything', function () {
-      var values = [
-        Object,
-        String,
-        Boolean,
-        Number,
-        Array,
-        Function,
-        function () {},
-        function test() {},
-        new Function(),
-        function test1() {},
-        function test2() {},
-        function test3() { },
-        function test4() { },
-        function/* foo*/test5() {},
-        function/* foo*/test6/* bar*/() {},
-        function/* foo*/test7/* bar*/(/* baz*/) {},
-        /* fum*/function/* foo*/ // blah
-            test8(/* baz*/
-             ) {}
-      ];
-      var expected = [
-        'Object',
-        'String',
-        'Boolean',
-        'Number',
-        'Array',
-        'Function',
-        '',
-        'test',
-        '',
-        'test1',
-        'test2',
-        'test3',
-        'test4',
-        'test5',
-        'test6',
-        'test7',
-        'test8'
-      ];
-      var actual = values.map(getFunctionName);
-      expect(actual).toEqual(expected);
-    });
-
-    ifSupportsFatit('should return a correct string for everything', function () {
-      var fat = getFat();
-      expect(getFunctionName(fat)).toBe('');
-    });
-
-    ifSupportsGFit('should return a correct string for everything', function () {
-      var gen = getGF();
-      expect(getFunctionName(gen)).toBe('idMaker');
-    });
-
-    ifSupportsAFit('should return a correct string for everything', function () {
-      var classes = getAF();
-      expect(getFunctionName(classes)).toBe('wait');
-    });
-
-    ifSupportsCit('should return a correct string for everything', function () {
-      var classes = getC();
-      expect(getFunctionName(classes)).toBe('My');
-    });
+    var cb = function () {};
+    var expected = values.map(cb);
+    var actual = values.map(getFunctionName);
+    expect(actual).toEqual(expected);
   });
-}());
+
+  it('should return a correct string for everything', function () {
+    var values = [
+      Object,
+      String,
+      Boolean,
+      Number,
+      Array,
+      Function,
+      function () {},
+      function test() {},
+      // eslint-disable-next-line no-new-func
+      new Function(),
+      function test1() {},
+      function test2() {},
+      function test3() { },
+      function test4() { },
+      function/* foo*/test5() {},
+      function/* foo*/test6/* bar*/() {},
+      function/* foo*/test7/* bar*/(/* baz*/) {},
+      /* fum*/function/* foo*/ // blah
+      test8(/* baz*/
+      ) {}
+    ];
+
+    var expected = [
+      'Object',
+      'String',
+      'Boolean',
+      'Number',
+      'Array',
+      'Function',
+      '',
+      'test',
+      '',
+      'test1',
+      'test2',
+      'test3',
+      'test4',
+      'test5',
+      'test6',
+      'test7',
+      'test8'
+    ];
+
+    var actual = values.map(getFunctionName);
+    expect(actual).toEqual(expected);
+  });
+
+  ifSupportsFatit('should return a correct string for everything', function () {
+    var fat = getFat();
+    expect(getFunctionName(fat)).toBe('');
+  });
+
+  ifSupportsGFit('should return a correct string for everything', function () {
+    var gen = getGF();
+    expect(getFunctionName(gen)).toBe('idMaker');
+  });
+
+  ifSupportsAFit('should return a correct string for everything', function () {
+    var classes = getAF();
+    expect(getFunctionName(classes)).toBe('wait');
+  });
+
+  ifSupportsCit('should return a correct string for everything', function () {
+    var classes = getC();
+    expect(getFunctionName(classes)).toBe('My');
+  });
+});
