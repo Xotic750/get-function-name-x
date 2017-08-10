f2619cd1f86973b4d0d706582486226d032d5bf7
Xotic750 <xotic750@gmail.com>
2017-08-10 03:15:37 +0100

    Upgrade whitespaces

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  * @file Get the name of the function.
- * @version 2.0.0
+ * @version 2.0.1
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
